042add07794fc0e8baa36b572a63143c23898e08
Richard Melchior <raustin.melchior@gmail.com>
2018-06-03 03:47:22 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,15 +1,27 @@
+##
+# => #sort_array_asc sorts the array in ascending order
+##
 def sort_array_asc(array)
   return array.sort;
 end
 
+##
+# => #sort_array_desc sorts the array in decending order
+##
 def sort_array_desc(array)
   return array.sort {|a, b| b <=> a};
 end
 
+##
+# => #sort_array_char_count sorts the array according to the number of characters from largest to smallest
+##
 def sort_array_char_count(array)
   return array.sort {|a, b| a.length <=> b.length};
 end
 
+##
+# => #swap_elements swaps the second and third elements in the array
+##
 def swap_elements(array)
   third_element = array[2];
   array[2] = array[1];
@@ -17,6 +29,9 @@ def swap_elements(array)
   return array;
 end
 
+##
+# => #swap_elements_from_to swaps the element at index with the element at destination_index
+##
 def swap_elements_from_to(array, index, destination_index)
   x_element = array[destination_index];
   array[destination_index] = array[index];
@@ -24,6 +39,7 @@ def swap_elements_from_to(array, index, destination_index)
   return array;
 end
 
+# personal tests for swap_elements_from_to due to the lack of tests provided
 puts swap_elements_from_to(["a", "b", "c"],0,2); #=> ["c", "b", "a"]
 puts "\n";
 puts swap_elements_from_to(["a", "b", "c"],2,1); #=> ["a", "c", "b"]
