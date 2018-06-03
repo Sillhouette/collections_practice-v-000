fb317d3647b49ebf24d34a9698e2acc722680ff9
Richard Melchior <raustin.melchior@gmail.com>
2018-06-03 03:31:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,4 +47,6 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect { | element, index | if index != 1 element + "s"}
+  array.each_with_index.collect { | element, index | if index != 1 element += "s"}
+  return array;
+end
