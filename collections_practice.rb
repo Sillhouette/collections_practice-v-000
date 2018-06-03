81e4904d12445ab2ad2c6c46142d66c9b9c180ee
Richard Melchior <raustin.melchior@gmail.com>
2018-06-03 03:20:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,3 +27,7 @@ end
 puts swap_elements_from_to(["a", "b", "c"],0,2) #=> ["c", "b", "a"]
 puts "\n"
 puts swap_elements_from_to(["a", "b", "c"],2,1) #=> ["a", "c", "b"]
+
+def reverse_array(array)
+  return array.reverse
+end
