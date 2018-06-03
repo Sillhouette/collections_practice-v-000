02bd62f1cc6792b2fc85fcb554ed7bf8b8f7a732
Richard Melchior <raustin.melchior@gmail.com>
2018-06-03 03:18:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -24,5 +24,5 @@ def swap_elements_from_to(array, index, destination_index)
   return array;
 end
 
-swap_elements_from_to(["a", "b", "c"],0,2) #=> ["c", "b", "a"]
-swap_elements_from_to(["a", "b", "c"],2,1) #=> ["a", "c", "b"]
+puts swap_elements_from_to(["a", "b", "c"],0,2) #=> ["c", "b", "a"]
+puts swap_elements_from_to(["a", "b", "c"],2,1) #=> ["a", "c", "b"]
